98dc38189679b29fdd1a0d1dfa8086ff70ffec43
GalinaGao123 <2901634865@qq.com>
2017-03-11 22:59:47 +0800

    live-chat

--- src/js/live-chat.js
+++ src/js/live-chat.js
@@ -1,5 +1,23 @@
 $(function(){
-  
+  //  手机pc切换
+  $('.welcome-items .control-btn').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    if ($(this).hasClass('pc')) {
+      $('.welcome .item-mobile').hide().siblings('.item-pc').fadeIn(300);
+    } else if ($(this).hasClass('mobile')) {
+      $('.welcome .item-pc').hide().siblings('.item-mobile').fadeIn(300);
+    }
+  });
+
+  $('.collaborate .control-btn').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    if ($(this).hasClass('pc')) {
+      $('.collaborate .item-mobile').hide().siblings('.col-pc').fadeIn(300);
+    } else if ($(this).hasClass('mobile')) {
+      $('.collaborate .col-pc').hide().siblings('.item-mobile').fadeIn(300);
+    }
+  });
+
   $('.slick-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -7,18 +25,39 @@ $(function(){
     fade: true,
     speed: 1000,
     prevArrow: '<div class="slick-prev"></div>',
-    nextArrow: '<div class="slick-next"></div>'
+    nextArrow: '<div class="slick-next"></div>',
+    responsive: [
+      {
+        breakpoint: 1180,
+        settings: {
+          arrows: false
+        } 
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          adaptiveHeight: true
+        }
+      }
+    ]
   });
 
   $('.slick-for').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    $('.tabs .tab').eq(nextSlide).addClass('current-tab').siblings().removeClass('current-tab');
+    $(this).siblings('.tabs').find('.tab').eq(nextSlide).addClass('current-tab').siblings().removeClass('current-tab');
   });
 
-  $('.tabs .tab').each(function(index){
-    $(this).click(function(){
-      $(this).addClass('current-tab').siblings().removeClass('current-tab');
-      $('.slick-for').slick('slickGoTo', index);
-    });
+  $('.tabs').each(function(){
+
+    var $that = $(this)
+
+    $that.find('.tab').each(function(index){
+
+      $(this).click(function(){
+        $(this).addClass('current-tab').siblings().removeClass('current-tab');
+        $that.siblings('.slick-for').slick('slickGoTo', index);
+      });
+
+    })
   });
 
 })
